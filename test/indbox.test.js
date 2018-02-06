4377eaf9de92f4014718603da86d507d26fe913c
Adrian Carolli <adrian.carolli1@gmail.com>
2018-02-06 10:59:55 -0500

    initial commit - basic deployment of inbox contract

---
+++ test/indbox.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+  // get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+    
+  // use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['hi there'] })
+    .send({ from: accounts[0], gas: '1000000'})
+    
+  inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+});
